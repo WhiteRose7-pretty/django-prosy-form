7620b1e609faa06c174f4c70539b6473e720604a
Ju Gui <cholpine@outlook.com>
2021-06-03 05:08:35 +0200

    add custom admin site setting

--- form/settings.py
+++ form/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3+cel^y-*)c9_yz+jkr3vd7vx238vwthd-(hdyo1(xcqg+=sgn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
@@ -36,7 +37,7 @@ INSTALLED_APPS = [
 	'rest_framework',
 	'corsheaders',
 	
-	'django.contrib.admin',
+	'form.apps.MyAdminConfig',  # replaces 'django.contrib.admin'
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
 	'django.contrib.sessions',
@@ -125,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
